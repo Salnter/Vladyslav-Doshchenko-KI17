9a6be0e5a42e0dfa626ca5dbc51f85d7826f122a
Vlad Doshchenko <hellowin_007@mail.ru>
2018-04-07 17:49:37 +0300

    Завершено модульне тестування функції q_calculate

--- lab2/Prj/TestDriver/main.cpp
+++ lab2/Prj/TestDriver/main.cpp
@@ -7,11 +7,22 @@ int main()
 {
     setlocale(LC_ALL,"ukr");
     double massiv[5];
-    double x,y,z,o;
-    x =1;
-    y=1;
-    z=1;
-    o = q_calculation(x,y,z) ;
-    cout<<o<<endl;
+   massiv[0]=q_calculation(1,1,1);
+    massiv[1]=q_calculation(1,2,1);
+     massiv[2]=q_calculation(1.05,1,1);
+      massiv[3]=q_calculation(-1,1,1);
+       massiv[4]=q_calculation(0,0,1);
+   if (massiv[0] == -0,168056462180752)
+    cout<<"TestCase #1"<<' '<<"Passed"<<endl;
+   if (massiv[1] != -0.23171843941751 )
+    cout<<"TestCase #2"<<' '<<"Passed"<<endl;
+    if (massiv[2] == -0,167829097976335)
+    cout<<"TestCase #3"<<' '<<"Passed"<<endl;
+    if (massiv[3] == -0,177151030357432)
+    cout<<"TestCase #4"<<' '<<"Passed"<<endl;
+    if (massiv[4] == -0,199887450799131)
+    cout<<"TestCase #5"<<' '<<"Passed"<<endl;
+
     return 0;
+
 }
